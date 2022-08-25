56f3d4f7c7dce9b4f6b4b1bb769859ad72fc462c
Fidan Oruc <fidanorucc>
2022-08-25 21:10:05 +0200

    Initial Commit

---
+++ LearningApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by Fidan Oruc on 25.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Fidan Oruc on 25.08.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
